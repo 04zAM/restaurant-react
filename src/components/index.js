fcf4fe5e5c52af63595e52910bbb46299c8443cf
04zAM <pathob13@gmail.com>
2023-05-08 21:23:04 +0000

    Code Synced by DhiWise

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import { Img } from "./Img";
+import { Text } from "./Text";
+import { Line } from "./Line";
+import { Button } from "./Button";
+import { RatingBar } from "./RatingBar";
+import { SelectBox } from "./SelectBox";
+import { Input } from "./Input";
+import { TextArea } from "./TextArea";
+import { List } from "./List";
+import { RadioGroup } from "./RadioGroup";
+import { Radio } from "./Radio";
+import { PagerIndicator } from "./PagerIndicator";
+import { Slider } from "./Slider";
+export {
+  Img,
+  Text,
+  Line,
+  Button,
+  RatingBar,
+  SelectBox,
+  Input,
+  TextArea,
+  List,
+  RadioGroup,
+  Radio,
+  PagerIndicator,
+  Slider,
+};
